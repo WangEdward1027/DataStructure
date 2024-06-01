4c90edf2b485eadaebd48de0cd1a4c414d1025f0
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-06-01 17:13:43 +0800

    哈希表的实现

--- HashTable/HashMap.c
+++ HashTable/HashMap.c
@@ -6,7 +6,7 @@
 #define DEFAULT_CAPACITY 8
 #define LOAD_FACTOR 0.75
 
-//1.创建
+//1.创建：创建空的哈希表
 HashMap* hashmap_create(void) {
 	HashMap* map = malloc(sizeof(HashMap));
 
@@ -23,11 +23,11 @@ void hashmap_destroy(HashMap* map) {
 	//1.释放所有的结点(遍历哈希表)
 	for (int i = 0; i < map->capacity; i++) {
 		Node* curr = map->table[i];         
-		while (curr) {
+		while (curr != NULL) {
 			Node* next = curr->next;
 			free(curr);
 			curr = next;
-		} // curr == NULL
+		} 
 	}
 	//2.释放动态数组
 	free(map->table);
@@ -67,7 +67,7 @@ static uint32_t hash(const void* key, int len, uint32_t seed) {
 
 //6.扩容
 void grow_capacity(HashMap* map) {
-	int new_capacity = 2 * map->capacity;
+	int new_capacity = 2 * map->capacity; 
 
 	Node** new_table = calloc(new_capacity, sizeof(Node*));  
 	if (!new_table) {
@@ -75,7 +75,7 @@ void grow_capacity(HashMap* map) {
 		exit(1);
 	}
 
-	//重新映射(rehash)
+	//重新映射(rehash)：扩容后，哈希表的容量改变了，每个键的哈希值和相应的索引位置也需要重新计算。
 	map->hashseed = time(NULL)-10086;
 
 	for (int i = 0; i < map->capacity; i++) {
@@ -84,9 +84,9 @@ void grow_capacity(HashMap* map) {
 			Node* next = curr->next;
 			//使用新的容量重新计算哈希值并找到新的索引
 			int new_idx = hash(curr->key, strlen(curr->key), map->hashseed) % new_capacity;
-			//将当前节点插入到新表的相应位置,头插法
-			curr->next = new_table[new_idx];
-			new_table[new_idx] = curr;
+			//将旧哈希表当前节点插入到新表的相应位置：头插法
+			curr->next = new_table[new_idx];	//curr下一个指向新位置
+			new_table[new_idx] = curr;			//新位置首元素就是curr结点，完成头插
 			curr = next;
 		}
 	}
@@ -96,7 +96,6 @@ void grow_capacity(HashMap* map) {
 	//更新结构体信息
 	map->capacity = new_capacity;
 	map->table = new_table;
-
 	return;
 }
 
@@ -125,10 +124,10 @@ V hashmap_put(HashMap* map, K key, V val) {
 		idx = hash(key, strlen(key), map->hashseed) % map->capacity; //重新计算索引
 	}
 
+	//创建结点
 	Node* new_node = malloc(sizeof(Node));
 	new_node->key = key;
 	new_node->val = val;
-
 	//头插法
 	new_node->next = map->table[idx];
 	map->table[idx] = new_node;
@@ -137,7 +136,7 @@ V hashmap_put(HashMap* map, K key, V val) {
 	return NULL;
 }
 
-//4.查找
+//4.查找：根据key，返回对应的value
 V hashmap_get(HashMap* map, K key) {
 	//对key进行哈希，判断key在哪个哈希桶中
 	int idx = hash(key, strlen(key), map->hashseed) % map->capacity;
