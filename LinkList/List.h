6e4f93807e3fb26fc39a02ba98e9d0353be5af4f
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-06-05 22:45:26 +0800

    Update List.h

--- LinkList/List.h
+++ LinkList/List.h
@@ -8,22 +8,23 @@ typedef struct node {
 typedef struct {
 	Node* head;
 	Node* tail;
-	int size;
+	E size;
 } List;
 
-// API
+//API
 List* list_create(void);
 void list_destroy(List* list);
 
-// 插入
-void add_before_head(List* list, E val);
-void add_hehind_tail(List* list, E val);
-void add_node(List* list, int idx, E val);
+//操作
+void add_before_head(List* list, E val);   //头插
+void add_behind_tail(List* list, E val);   //尾插
+void add_node(List* list, int idx, E val); //任意位置插入结点
 
 // 删除第一个值与val相等的结点
 void delete_node(List* list, E val);
 
 // 根据索引查找值
 Node* find_by_index(List* list, int idx);
+
 // 查找第一个与指定值相等的结点
 Node* search_for_value(List* list, E val);
