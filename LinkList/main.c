e506ee4568d6f0cb58e31efe1c549087ebf31dcd
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-06-05 22:44:42 +0800

    Update main.c

--- LinkList/main.c
+++ LinkList/main.c
@@ -4,27 +4,29 @@
 
 int main(void) {
 	List* list = list_create();
+	//add_before_head(list, 1);
+	//add_before_head(list, 2);
+	//add_before_head(list, 3);
+	//add_before_head(list, 4);	   // 4 --> 3 --> 2 --> 1
 
-	// add_before_head(list, 1);
-	// add_before_head(list, 2);
-	// add_before_head(list, 3);
-	// add_before_head(list, 4);	// 4 --> 3 --> 2 --> 1
+	add_behind_tail(list, 1);
+	add_behind_tail(list, 2);
+	add_behind_tail(list, 3);
+	add_behind_tail(list, 4);	   // 1 --> 2 --> 3 --> 4
 
-	add_hehind_tail(list, 1);
-	add_hehind_tail(list, 2);
-	add_hehind_tail(list, 3);
-	add_hehind_tail(list, 4);	// 1 --> 2 --> 3 --> 4
-
-	// add_node(list, 0, 100);
-	// add_node(list, 3, 200);
-	// add_node(list, 6, 300);		// 100 -> 1 -> 2 -> 200 -> 3 -> 4 -> 300
-	// add_node(list, 10, 400);
+	 add_node(list, 0, 100);
+	 add_node(list, 3, 200);
+	 add_node(list, 6, 300);		// 100 -> 1 -> 2 -> 200 -> 3 -> 4 -> 300
+	 //add_node(list, 10, 400);
 
 	// delete_node(list, 3);
 	// delete_node(list, 1);
 	// delete_node(list, 10);
 
-	Node* node1 = find_by_index(list, 2);
-	Node* node2 = find_by_index(list, 10);
-	return 0;
+	 Node* node1 = find_by_index(list, 0);
+	 printf("data = %d\n", node1->data);
+	 Node* node2 = find_by_index(list, 6);
+	 printf("data = %d\n", node2->data);
+	 Node* node3 = find_by_index(list, 10);
+	 return 0;
 }
